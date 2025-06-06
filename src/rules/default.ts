5f306429ea649672f2e4ecdda337b671820bf326
Yoeri Nijs <yoeri@posteo.net>
2025-06-06 22:05:55 +0200

    Add some rules

---
+++ src/rules/default.ts
@@ -0,0 +1,50 @@
+import {PhishingRules} from "../model/phishing-rules";
+
+const DEFAULT_RULES: PhishingRules = {
+    include: [
+        {
+            name: 'bank_include_rule_1',
+            description: 'Default phishing detection rule for banks',
+            value: 'bank',
+            weight: 0.5,
+            phishingRuleType: 'contains'
+        },
+        {
+            name: 'bank_include_rule_2',
+            description: 'Default phishing detection rule for banks',
+            value: 'internetbankieren',
+            weight: 0.5,
+            phishingRuleType: 'contains'
+        }
+    ],
+    exclude: [
+        {
+            name: 'bank_exclude_rule_1',
+            description: 'Default phishing detection rule for banks',
+            value: 'rabobank.nl',
+            weight: 0.5,
+            phishingRuleType: 'contains'
+        },
+        {
+            name: 'bank_exclude_rule_2',
+            description: 'Default phishing detection rule for banks',
+            value: 'ing.nl',
+            weight: 0.5,
+            phishingRuleType: 'contains'
+        },
+        {
+            name: 'bank_exclude_rule_3',
+            description: 'Default phishing detection rule for banks',
+            value: 'bunq.nl',
+            weight: 0.5,
+            phishingRuleType: 'contains'
+        },
+        {
+            name: 'bank_exclude_rule_4',
+            description: 'Default phishing detection rule for banks',
+            value: 'abnamro.nl',
+            weight: 0.5,
+            phishingRuleType: 'contains'
+        },
+    ]
+}
